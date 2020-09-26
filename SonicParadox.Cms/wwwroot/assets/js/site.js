47168c834019980b28d83cb93a6f7cad83dab540
halomademeapc <alex@halomademeapc.com>
2020-09-25 21:33:07 -0400

    start styling navbar

--- SonicParadox.Cms/wwwroot/assets/js/site.js
+++ SonicParadox.Cms/wwwroot/assets/js/site.js
@@ -29,4 +29,30 @@ class StaffPopoutController {
     }
 }
 
+class ScrollCollapseController {
+    binder;
+    element;
+    scrollPosition;
+    deadzone = 36;
+
+    constructor(element) {
+        this.element = element;
+        this.binder = new GravyBinder(this, this.element);
+        this.bindEvents();
+        this.updatePosition();
+    }
+
+    bindEvents = () => {
+        window.addEventListener('scroll', this.updatePosition);
+    }
+
+    updatePosition = () => {
+        this.scrollPosition = window.scrollY;
+        this.binder.updateOutwardBindings();
+    }
+
+    isScrolled = () => this.scrollPosition > this.deadzone;
+}
+
 loopThroughElements(document.getElementsByClassName('staff'), element => element.staffController = new StaffPopoutController(element));
+loopThroughElements(document.getElementsByClassName('scroll-trigger'), element => element.scrollController = new ScrollCollapseController(element));
